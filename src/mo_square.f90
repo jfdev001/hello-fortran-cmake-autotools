fe4533f64cb51ed485143a50664062507e2d6e09
Jared Frazier <frazier@iap-kborn.de>
2025-04-11 15:26:00 +0200

    successful linking of autoconf built dependency!

--- src/mo_square.f90
+++ src/mo_square.f90
@@ -1,11 +1,16 @@
 MODULE mo_square
-    !USE mo_rte_kind, ONLY: wp !< Make sure target link works
+    USE mo_rte_kind, ONLY: wp !< Make sure target link works
     IMPLICIT NONE
     PRIVATE
     PUBLIC :: square
+    PUBLIC :: verify_mo_rte_kind
 CONTAINS
     PURE REAL FUNCTION square(x) result(r)
         REAL, INTENT(in) :: x  !< Input real number.
         r = x**2
     END FUNCTION square
+
+    SUBROUTINE verify_mo_rte_kind()
+        print *, "mo_rte_kind::wp = ", wp 
+    END SUBROUTINE verify_mo_rte_kind
 END MODULE mo_square
